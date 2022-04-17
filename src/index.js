9af3ee7f685b63e319994aeb25a160c15c3a80b0
BossDaily <comparatorcraft2020@gmail.com>
2022-04-17 12:33:58 -0500

    Did the project I guess

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Create a reat component
+
+
+// Take the react component and show it on the screen
+
+const App = () => {
+    const buttonText = { text:'click meee' }
+
+    return (
+        <div>
+            <label className="label" htmlFor="name" > Enter Something:</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}> {buttonText.text} </button>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)

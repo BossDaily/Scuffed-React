a8a82dff96ee865e63a6a4eba768954baa4ddb8d
gboys2008@gmail.com <gboys2008@gmail.com>
2022-04-18 11:56:44 -0400

    new accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: 'What is CCSMP?',
+    content: 'CCSMP is the best mc server of all time'
+  },
+  {
+    title: 'Why CCSMP',
+    content: 'Because we are goated' 
+  },
+  {
+    title: 'Among us',
+    content: 'Mid game'
+  }
+]
 
 const App = () => {
   return(
     <h1>
       Fard
+      <Accordion items={items} />
     </h1>
   )
 }

b06da6c6cc554f886cc5db5208f3888bbd108563
gboys2008@gmail.com <gboys2008@gmail.com>
2022-04-18 11:57:04 -0400

    renders content from items prop

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,27 @@
 import React from "react";
+import './Gradient.css'
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+        return (
+            <div key={item.title}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </div>
+        )
+    })
 
+    return (
+        <div className="ui styled accordion" >
+            {renderedItems}
+        </div>
+
+    )
 }
 
 export default Accordion

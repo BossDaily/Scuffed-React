879c3b5d2514bedee9a2c4a62e2fdf729227773b
gboys2008@gmail.com <gboys2008@gmail.com>
2022-04-21 11:12:30 -0400

    fard

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,30 +1,21 @@
-import React from "react";
-import './Gradient.css'
-import { Accordion } from 'flowbite-react'
+import React from 'react';
 
-const AccordionList = ({ items }) => {
-    const renderedItems = items.map(item => {
-        return (
-            <Accordion>
-                <Accordion.Panel open={true}>
-                    <Accordion.Title arrowIcon={undefined}>
-                        <i className="dropdown icon"></i>
-                        {item.title}
-                    </Accordion.Title>
-                    <Accordion.Content>
-                        <p>{item.content}</p>
-                    </Accordion.Content>
-                </Accordion.Panel>
-            </Accordion>
-        )
-    })
-
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
     return (
-        <div className="ui styled accordion" >
-            {renderedItems}
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-    )
-}
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
-export default AccordionList
+export default Accordion;
